827f3323820b86fbfb02f1d9ebe082a86e77db90
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-05-17 10:42:50 +0800

    Nav bar now has content

--- app/ui/sticky_nav.tsx
+++ app/ui/sticky_nav.tsx
@@ -22,9 +22,25 @@ export default function StickyNav() {
 
   return (
     <nav
-      className={`sticky top-0 z-50 transition-transform duration-300 bg-white border-b border-gray-200 p-4 ${
+      className={`sticky top-0 z-50 transition-transform duration-300 bg-white border-b border-gray-200 p-4 flex items-center justify-between ${
         visible ? "translate-y-0" : "-translate-y-full"
       }`}
-    ></nav>
+    >
+      <img src="logo.png" alt="Website logo" className="w-30" />
+      <div className="flex w-auto">
+        <a href="#home" className="mx-2 text-gray-700 hover:text-gray-900">
+          Home
+        </a>
+        <a href="#about" className="mx-2 text-gray-700 hover:text-gray-900">
+          About
+        </a>
+        <a href="#services" className="mx-2 text-gray-700 hover:text-gray-900">
+          Services
+        </a>
+        <a href="#contact" className="mx-2 text-gray-700 hover:text-gray-900">
+          Contact
+        </a>
+      </div>
+    </nav>
   );
 }
