2a0ef93086e8575205afb15634b8f392daa1460e
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-05-21 23:50:58 +0800

    Updated page.tsx
    Added some content to the home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
 export default function Home() {
-  return;
+  return (
+    <>
+      <div className="grid grid-cols-2 items-end">
+        <div className="m-20">
+          <p className="w-100 font-bold text-[#242C5B] dark:text-[#A5B4FC]">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam.
+          </p>
+          <h1 className="pt-7 text-8xl font-black">
+            <span className="text-[#0E227A]">Finally</span>
+            <br />
+            <span className="text-[#1957C4]">500 Years!</span>
+          </h1>
+          <button className="mt-7">learn more</button>
+        </div>
+        <img
+          src="Home page background 1.png"
+          alt="Home page background"
+          className="h-screen"
+        />
+      </div>
+    </>
+  );
 }
