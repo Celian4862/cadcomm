c411d96607daa156be0bc2b742d639d53938947a
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-05-22 16:53:11 +0800

    Updated nav_bar.tsx
    Changed the anchor tags to Link tags.
    
    Changed the img tag to an Image tag.

--- app/ui/nav_bar.tsx
+++ app/ui/nav_bar.tsx
@@ -1,16 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function NavBar() {
   return (
-    <div className="bg-bg-primary/40 absolute top-5 z-50 flex w-full items-center justify-between p-2 shadow-[0_2px_4px_-1px_rgba(0,0,0,0.15),0_-2px_12px_-1px_rgba(0,0,0,0.15)] transition-transform duration-300">
-      <a href="/">
-        <img src="logo.png" alt="Website logo" className="w-60" />
-      </a>
-      <nav className="*:text-secondary flex w-auto justify-around gap-x-15 pr-10 *:text-[1.2rem] *:uppercase *:transition-colors *:duration-300 *:hover:text-[#7dd3fc] *:active:text-[#1a1a6b] *:dark:text-[#7dd3fc] *:dark:hover:text-[#ffffff] *:dark:active:text-[#38bdf8]">
-        <a href="/about-us">about us</a>
-        <a href="/curia">curia</a>
-        <a href="/clergy">clergy</a>
-        <a href="/pilgrimages">pilgrimages</a>
-        <a href="/archdiocesan-institutions">archdiocesan institutions</a>
-        <a href="/updates">updates</a>
+    <div className="bg-bg-primary/85 absolute top-5 z-50 flex w-full items-center justify-between p-2 shadow-[0_2px_4px_-1px_rgba(0,0,0,0.15),0_-2px_12px_-1px_rgba(0,0,0,0.15)] transition-transform duration-300">
+      <Link href="/">
+        <Image src="/logo.png" alt="Website logo" width={250} height={0} />
+      </Link>
+      <nav className="*:text-secondary flex w-auto justify-around gap-x-15 pr-10 *:text-[1.2rem] *:uppercase *:transition-colors *:duration-300 *:hover:text-[#7dd3fc] *:active:text-[#1a1a6b] *:dark:hover:text-[#ffffff] *:dark:active:text-[#38bdf8]">
+        <Link href="/about-us">about us</Link>
+        <Link href="/curia">curia</Link>
+        <Link href="/clergy">clergy</Link>
+        <Link href="/pilgrimages">pilgrimages</Link>
+        <Link href="/archdiocesan-institutions">archdiocesan institutions</Link>
+        <Link href="/updates">updates</Link>
       </nav>
     </div>
   );
