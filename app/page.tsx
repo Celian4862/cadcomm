2f47f06e3558bee4cd3c0d3ffe4b2ba18c90a4c2
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-05-23 16:42:05 +0800

    Edited design responsiveness, moved the home page Image back into the first grid

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <Image
-        src="/Home page background 1.png"
-        alt="Home page background"
-        width={300}
-        height={300}
-        className="absolute top-0 right-0 h-0 w-0 lg:h-screen lg:w-[50vw]"
-      />
-      <div className="lg:grid lg:h-screen lg:grid-cols-1 lg:items-end">
+      <div className="sm:grid sm:grid-cols-2 lg:h-screen lg:grid-cols-1 lg:items-end">
         <div className="m-10 lg:m-20">
-          <p className="font-bold lg:w-100 dark:text-[#A5B4FC]">
+          <p className="font-bold sm:w-100 dark:text-[#A5B4FC]">
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
             nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam.
           </p>
@@ -23,14 +16,21 @@ export default function Home() {
           </p>
           <button className="mt-7">learn more</button>
         </div>
+        <Image
+          src="/Home page background 1.png"
+          alt="Home page background"
+          width={300}
+          height={300}
+          className="hidden h-full w-full sm:block lg:absolute lg:top-0 lg:right-0 lg:block lg:h-screen lg:w-[50vw]"
+        />
       </div>
       <header>
         <h2 className="bg-secondary w-full p-4 text-center text-4xl font-bold tracking-widest text-white uppercase lg:text-5xl">
           History
         </h2>
       </header>
-      <div className="grid grid-cols-1 gap-y-5 px-10 pt-5 lg:grid-cols-3 lg:gap-x-13 lg:px-15 lg:pt-10">
-        <div className="bg-bg-primary/40 p-5 shadow-[black_0_2px_10px]/15 lg:col-span-2 lg:m-0">
+      <div className="grid grid-cols-1 gap-y-5 px-10 pt-5 md:grid-cols-2 md:gap-x-13 md:px-15 md:pt-10 lg:grid-cols-3">
+        <div className="bg-bg-primary/40 p-5 shadow-[black_0_2px_10px]/15 md:col-span-1 md:m-0 lg:col-span-2">
           <h3 className="text-tertiary mb-5 text-2xl font-bold">
             Archdiocese of Cebu: The Journey of Transformative Faith and Hope in
             the Orient
