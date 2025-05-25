bf645e0486c724b9c9886acc8f48f141d3db6743
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-05-25 23:48:28 +0800

    Updated home_button.tsx
    HomeButton can now accept the prop className so that extra styles may be applied to specific buttons.

--- app/ui/home_button.tsx
+++ app/ui/home_button.tsx
@@ -1,10 +1,14 @@
 export default function HomeButton({
   children,
+  className = "",
 }: {
   children: React.ReactNode;
+  className?: string;
 }) {
   return (
-    <button className="hover:bg-bg-primary relative my-6 rounded-4xl border border-solid border-(--foreground) bg-(--background) px-8 py-3 text-xs font-extrabold tracking-widest uppercase shadow-[0_3px_2px_var(--button-shadow)] active:top-0.5">
+    <button
+      className={`${className} hover:bg-bg-primary relative my-6 rounded-4xl border border-solid border-(--foreground) bg-(--background) px-8 py-3 text-xs font-extrabold tracking-widest uppercase shadow-[0_3px_2px_var(--button-shadow)] active:top-0.5`}
+    >
       {children}
     </button>
   );
