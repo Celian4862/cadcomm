6ced7d2cb5a3850ca9a32c53b4705c757f52bb1f
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-05-26 00:03:33 +0800

    Updated home_button.tsx
    Modified the style of the active button

--- app/ui/home_button.tsx
+++ app/ui/home_button.tsx
@@ -7,7 +7,7 @@ export default function HomeButton({
 }) {
   return (
     <button
-      className={`${className} hover:bg-bg-primary relative my-6 rounded-4xl border border-solid border-(--foreground) bg-(--background) px-8 py-3 text-xs font-extrabold tracking-widest uppercase shadow-[0_3px_2px_var(--button-shadow)] active:top-0.5`}
+      className={`${className} hover:bg-bg-primary relative my-6 rounded-4xl border border-solid border-(--foreground) bg-(--background) px-8 py-3 text-xs font-extrabold tracking-widest uppercase shadow-[0_3px_2px_var(--button-shadow)] active:top-0.5 active:shadow-none`}
     >
       {children}
     </button>
