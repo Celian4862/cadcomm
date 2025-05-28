1b6b8dff0489c5870e0cbac2e6d6b7cd1965a556
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-05-28 14:47:42 +0800

    Updated news_card.tsx
    In the News Updates section: centred the text while keeping the buttons at the bottom of the div.

--- app/ui/news_card.tsx
+++ app/ui/news_card.tsx
@@ -9,11 +9,17 @@ export default function NewsCard({
 }) {
   return (
     <div
-      className="h-120 bg-cover bg-center"
+      className="relative flex h-120 flex-col justify-end bg-cover bg-center"
       style={{ backgroundImage: `url("${img_url}")` }}
     >
-      <div className="p-5 text-3xl font-bold uppercase">{text}</div>
-      <HomeButton>Read more</HomeButton>
+      <div className="absolute top-0 left-0 flex h-full w-full items-center justify-center">
+        <div className="p-5 text-center text-3xl font-bold uppercase">
+          {text}
+        </div>
+      </div>
+      <div className="p-5">
+        <HomeButton>Read more</HomeButton>
+      </div>
     </div>
   );
 }
