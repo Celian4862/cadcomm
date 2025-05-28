6fff94fbe8b1e454ab9b79423ab563d4fe61a9a5
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-05-28 23:15:08 +0800

    Several changes occurred
    Added a new route for /about-us which redirects to /about-us/history-timeline
    
    Added content for the History Timeline page.

---
+++ app/about-us/history-timeline/page.tsx
@@ -0,0 +1,133 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <section>
+      <header>
+        <h1 className="text-tertiary mb-10 text-5xl font-bold uppercase">
+          Previous Bishops of Cebu
+        </h1>
+      </header>
+      <section>
+        <header>
+          <h2 className="text-tertiary mb-5 text-5xl font-bold uppercase">
+            Gabriel Reyes
+          </h2>
+        </header>
+        <div className="text-justify">
+          <div className="gap-7 sm:grid sm:grid-cols-3">
+            <div className="col-span-2">
+              <p>
+                Archbishop Gabriel M. Reyes was born on March 24, 1892, in
+                Kalibo, Capiz, to Filipino parents. At the age of 13, he entered
+                the Seminary of Jaro which was then directed by the Vincentian
+                Fathers. Bishop Dennis Dougherty of Jaro, who later became
+                Cardinal Archbishop of Philadelphia, U.S.A., ordained him on
+                March 27, 1915, after which he was immediately appointed
+                Coadjutor Parish Priest of the Cardinal becoming in a few months
+                its Parish Priest.
+              </p>
+              <p>
+                As Parish Priest of Balasan (1915) he covered sixteen small
+                islands many of which of neither roads nor chapels nor convents,
+                but only ruins and an increasing number of Aglipayans and
+                Protestants. These became the field of action of the energetic
+                Gabriel until he became a Parish Priest of Capiz, Capiz (1918).
+              </p>
+              <p>
+                On July 20, 1820, Bishop James McClosky chose him to be the
+                Diocesan Chancellor and Secretary and, at the same time, a
+                Parish Priest of Sta. Barbara. In 1927, he became Vicar General
+                of Jaro.
+              </p>
+              <p>
+                On October 11, 1932, Monsignor Gabriel Reyes was ordained Bishop
+                of Cebu in the Cardinal of Jaro by the Apostolic Nuncio,
+                Archbishop Guillermo Piani, S.D.B., with the assistance of
+                Bishop James McClosky of Jaro and Bishop Alfredo Verzosa of
+                Lipa. Two days later, he was installed in Cebu.
+              </p>
+              <p>
+                On April 28, 1934, Cebu, after more than three centuries was
+                elevated by Pope Pius XI into an Archdiocese with Bishop Gabriel
+                Reyes as the First Archbishop. As the Ecclesiastical Province of
+                Santisimo Nombre de Jesus Cebu had for its suffragans in 1934
+                the following: the Diocese of Jaro (1865), Calbayog (1910),
+                Zamboanga (1910), Bacolod (1932 detached from Manila), and
+                Cagayan de Oro (1933).
+              </p>
+            </div>
+            {/* 2025.05.28, 23:13: There should be a way to make the text
+            automatically wrap around the image instead of statically forcing
+            the text and the image to be part of a grid. */}
+            <Image
+              src="/gabriel-reyes.png"
+              alt="Gabriel Reyes"
+              width={500}
+              height={500}
+              className="mb-5 rounded-lg shadow-lg"
+            />
+          </div>
+          <p>
+            With the able hands of Archbishop Gabriel Reyes, the Archdiocese
+            began to embark on a series of noble projects designed to nurture
+            the faith that had already been planted. In September of 1934, the
+            weekly newspaper Lungsuranon appeared in its maiden issue. It was to
+            help instruct the faithful on the fundamentals of the faith and to
+            hold in check the advances of the other sectarian propaganda. The
+            Archbishop encouraged the establishment of “Catholic Action” in the
+            Parishes of Cebu and Bohol as well as the Catechesis for children
+            and the Association of Propagation of Faith. He ordered the holding
+            of Diocesan Eucharistic Congresses in all the Parishes of the
+            Archdiocese for the preparation of the International Eucharistic
+            Congress which was scheduled in Manila in February of 1937. In 1933
+            he established in Cebu the Adoracion Nocturna Filipina and in 1940
+            the Knights of Columbus. On July 27 - 29, 1937 he convoked the Third
+            Diocesan Synod of Cebu where decrees adjusted to the new code of
+            Canon Law (1918) were adopted. Truly a pastor of souls, Archbishop
+            Reyes himself handled spiritual exercises for the common laborers,
+            with hundreds of them attending every year.
+          </p>
+          <p>
+            Giving concrete responses to the evangelical imperative in works of
+            mercy, Archbishop Reyes laid the foundation stone in 1939 for a
+            large structure of the Asilo de Milagrosa to take care of the
+            orphans and the aged. He supported the Association of St. Vincent de
+            Paul to help the poor and the needy. Furthermore, he erected the
+            now-defunct Holy Child Hospital with the same purpose in mind.
+          </p>
+          <p>
+            To mark the celebration of his Sacerdotal Silver Jubilee in 1940,
+            Archbishop Reyes embellished the Cathedral erecting therein a marble
+            altar which unfortunately did not survive the war. He encouraged
+            religious vocation among the youth and ordained every year dozens of
+            new candidates to the priesthood. He established the Parishes of
+            Guadalupe, Tabuelan, Simala, and Santa Lucia to add to the existing
+            parish, and launched a more vigorous evangelization program. He
+            started the “Catholic Hour” over the radio station DZRC.
+          </p>
+          <p>
+            In 1945 the Treaty of Peace was signed. The Archbishop, pained
+            almost to the point of nervous breakdown by the great destruction
+            caused by the Second World War, such as the destruction of the
+            Cathedral, his residence, the seminary, several churches, convents,
+            and colleges, resigned to the will of God, commenced the work of
+            reconstruction. His labor did not pass unrecognized for on August
+            25, 1949, he was appointed by the Holy Father Pius XII as Coadjutor
+            to Archbishop Michael O'Doherty of Manila with right of succession
+            (and Apostolic Administrator sede plena). With the death of the
+            Archbishop, he took canonical possession of the archiepiscopal See
+            of Manila with the distinction of being its first Filipino
+            Archbishop (September 29, 1949).
+          </p>
+          <p>
+            Archbishop Gabriel Reyes has the distinction of being succeeded by
+            two who have become Princes of the Church: Julio Cardinal Rosales
+            who succeeded him in Cebu and Rufino Cardinal Santos who succeeded
+            him in Manila.
+          </p>
+        </div>
+      </section>
+    </section>
+  );
+}
---
+++ app/about-us/layout.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="p-5 lg:grid lg:grid-cols-4 lg:gap-5">
+      <div>
+        <aside
+          className="bg-bg-primary/40 hidden p-5 lg:block"
+          aria-label="Mini navigation"
+        >
+          <h2 className="text-tertiary m-5 mt-0 text-3xl font-bold uppercase">
+            About Us
+          </h2>
+          <div className="flex flex-col justify-between gap-2 *:rounded-xl *:px-5 *:py-2">
+            <Link
+              href="/about-us/history-timeline"
+              className="text-bg-primary bg-tertiary"
+            >
+              History Timeline
+            </Link>
+            <Link
+              href="/about-us/archdioceses-coat-of-arms"
+              className="bg-bg-primary text-secondary"
+            >
+              Archdiocese's Coat of Arms
+            </Link>
+          </div>
+        </aside>
+      </div>
+      <div className="lg:col-span-3">{children}</div>
+    </div>
+  );
+}
