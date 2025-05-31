b7f7889bfa6de9ce2ab40feea72ffeb918510796
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-05-31 13:20:53 +0800

    Updated sidebar_nav.tsx
    Modified the styles of the navigation links

--- app/ui/sidebar_nav.tsx
+++ app/ui/sidebar_nav.tsx
@@ -21,7 +21,7 @@ export default function SidebarNav() {
       <h2 className="text-tertiary m-5 mt-0 text-3xl font-bold uppercase">
         About Us
       </h2>
-      <div className="flex flex-col justify-between gap-2 *:rounded-xl *:px-5 *:py-2">
+      <div className="flex flex-col justify-between gap-2 font-light *:rounded-xl *:px-5 *:py-2">
         {links.map((link) => (
           <Link
             key={link.href}
@@ -29,7 +29,7 @@ export default function SidebarNav() {
             className={
               pathname === link.href
                 ? "text-bg-primary bg-tertiary"
-                : "bg-bg-primary text-secondary"
+                : "bg-bg-primary text-secondary border border-solid border-black/20"
             }
           >
             {link.label}
