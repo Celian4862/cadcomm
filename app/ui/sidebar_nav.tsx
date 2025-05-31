5837f9f6653c90fd2ce326037c0ce1f34f76d2cf
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-05-31 13:12:31 +0800

    Added sidebar_nav.tsx
    Created a client component in order to separately style the Link that matches the current path name.

---
+++ app/ui/sidebar_nav.tsx
@@ -0,0 +1,41 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  { href: "/about-us/history-timeline", label: "History Timeline" },
+  { href: "/about-us/archbishop-of-cebu", label: "Archbishop of Cebu" },
+  {
+    href: "/about-us/archdioceses-coat-of-arms",
+    label: "Archdiocese's Coat of Arms",
+  },
+];
+
+export default function SidebarNav() {
+  const pathname = usePathname();
+  return (
+    <aside
+      className="bg-bg-primary/40 hidden p-5 lg:block"
+      aria-label="Mini navigation"
+    >
+      <h2 className="text-tertiary m-5 mt-0 text-3xl font-bold uppercase">
+        About Us
+      </h2>
+      <div className="flex flex-col justify-between gap-2 *:rounded-xl *:px-5 *:py-2">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={
+              pathname === link.href
+                ? "text-bg-primary bg-tertiary"
+                : "bg-bg-primary text-secondary"
+            }
+          >
+            {link.label}
+          </Link>
+        ))}
+      </div>
+    </aside>
+  );
+}
