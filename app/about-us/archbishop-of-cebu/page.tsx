0a5019cd0cb301cd4f6450c84c363165fb65bc07
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-05-31 15:31:56 +0800

    Added about-us/archbishop-of-cebu/page.tsx

---
+++ app/about-us/archbishop-of-cebu/page.tsx
@@ -0,0 +1,131 @@
+import FloatingImage from "@/app/ui/floating_image";
+
+export default function Page() {
+  return (
+    <section>
+      <header>
+        <h1 className="mb-10 text-left text-3xl font-black uppercase md:text-5xl">
+          <div className="text-tertiary">The Archbishop</div>
+          <div className="text-quaternary">Most Rev. Jose S. Palma, DD</div>
+        </h1>
+      </header>
+      <div className="text-justify">
+        <FloatingImage
+          src="archbishop-palma.png"
+          alt_text="Archbishop Jose S. Palma"
+        />
+        <p>
+          Jose S. Palma was born on March 19, 1950 to Ruperto and Ester in the
+          sleepy town of Dingle, Iloilo. He was raised in the simple town
+          located 41 kilometers from the city, characterized by its natural
+          beauty, vast rice fields and rolling mountains
+        </p>
+        <p>
+          Affectionately called “Nono” by his loved ones, he is the second of
+          eight children who all grew up in a humble and grounded family,
+          anchored in love and respect. Both his parents brought them up in the
+          Catholic faith, and his big happy family served as the foundation of
+          his faith in God-- young Nono saw God&apos;s love through them.
+        </p>
+        <p>
+          Jose&apos;s father served as the barangay chief who was greatly
+          admired by the people. His Tatay Ruperto was of great influence to the
+          future archbishop, who raised him and his siblings with the virtues of
+          humility, kindness, and faith.
+        </p>
+        <p>
+          Jose grew up as a solemn and silent young boy who also excelled in
+          school. He finished his primary education at Calicuang Elementary
+          School where he graduated as class valedictorian, and secondary
+          education at Mater Carmeli School, Iloilo where his faith formation
+          was strengthened by the Carmelite sisters.
+        </p>
+        <p>
+          Jose was initially interested in taking up Engineering, however, his
+          faith and the call to priesthood proved stronger and louder. His
+          “fiat” or yes to God&apos;s call became a prelude to an extraordinary
+          life of service to the will of God.
+        </p>
+        <p>
+          Firm in offering himself to the altar of priesthood, he studied
+          philosophy at St. Vincent Ferrer Seminary and was among the first
+          graduates at St. Joseph Regional Seminary where he studied theology.
+          He was ordained on August 21, 1976, the 3rd anniversary of his
+          father&apos;s death, a heartfelt demonstration of his great love and
+          admiration for his Tatay Ruperto.
+        </p>
+        <p>
+          Fr. Palma proceeded to study Licentiate in Sacred Theology (Magna Cum
+          Laude) at the University of Santo Tomas, Manila from 1977-1980. After
+          completing his studies, he returned to St. Joseph Seminary and served
+          as a professor and vice-rector until 1983. He also finished his
+          Doctorate in Theology (Magna Cum Laude) at the Pontifical University
+          of St. Thomas Aquinas in Rome. He returned to the Seminary once again
+          to serve as its rector from 1988-1997.
+        </p>
+        <p>
+          An approachable young priest who mingled with his flock, Fr. Palma
+          then expanded his pastoral service as the parish priest of St. Anthony
+          of Padua Parish in Barotac Nuevo, Iloilo from 1997-1998. He was later
+          appointed Auxiliary Bishop of Cebu from 1998-1999 and Bishop of
+          Calbayog from 1999-2006, where he notably spearheaded the
+          medico-surgical mission for the poor.
+        </p>
+        <p>
+          In 2006, he was appointed by Pope Benedict XVI as Archbishop of Palo,
+          Leyte. During his tenure, he visited the diocese&apos;s far-flung
+          outposts and the peripheries to administer the sacraments and aid
+          elderly priests.
+        </p>
+        <p>
+          He is a “silent worker and action man” who wants to serve and spend
+          time with the people. Known as a humble “barangay bishop”, he
+          frequently visits the slums and walks the hinterlands to listen to the
+          stories of the voiceless and marginalized, and preach the message of
+          God along the way.
+        </p>
+        <p>
+          On January 13, 2011 he was installed as the fourth Archbishop of Cebu,
+          continuing the work of Ricardo Cardinal Vidal who led the Archdiocese
+          for more than 28 years. Although the urbanized Archdiocese proved to
+          be quite intimidating for the new Shepherd from Samar, he immediately
+          went to work, laying down his vision for the Archdiocese as a big
+          family, empowering the faithful, ushering in reforms, and hearing the
+          concerns of both religious and laypersons.
+        </p>
+        <p>
+          Although leading the country&apos;s largest Archdiocese may have its
+          share of trials and challenges, the new Archbishop felt comfortable in
+          his new post with the immense welcome of the Cebuanos and the support
+          of his many collaborators who were willing to help him in his
+          ministry.
+        </p>
+        <p>
+          In the same year, he was elected president of the Catholic
+          Bishops&apos; Conference of the Philippines where he served until
+          2013. Accepting his appointment as CBCP head, he wrote: “unworthy as I
+          am, I accepted the appointment out of obedience”- a statement which
+          could be likened to his namesake, St. Joseph&apos;s acceptance to be
+          Mary&apos;s spouse.
+        </p>
+        <p>
+          Archbishop Palma recognizes technology as an effective means of social
+          communication at the dawn of a new age. He is known to be tech-savvy,
+          looking out for his brother priests, religious and lay faithful by
+          communicating with them through text and email. He is also a staunch
+          protector of the sanctity of marriage, family and life, speaking out
+          against issues involving faith and morality, and renouncing worldly
+          joys like wealth, fame and power.
+        </p>
+        <p>
+          As Archbishop of Cebu, he spearheaded the successful celebration of
+          the canonization of Visayan martyr, Pedro Calungsod in 2012, and the
+          51st International Eucharistic Congress in 2016. Under his leadership,
+          the Archdiocese will again host big ecclesiastical gatherings in 2021
+          with the Quincentennial Anniversary of the Philippine
+          Christianization.
+        </p>
+      </div>
+    </section>
+  );
+}
