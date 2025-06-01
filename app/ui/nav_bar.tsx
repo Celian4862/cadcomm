4dd156c4fb4fc7314f6dad46b107bab0060fee88
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-06-01 14:11:58 +0800

    Updated nav_bar.tsx
    The nav tag now encompasses the home button as well.

--- app/ui/nav_bar.tsx
+++ app/ui/nav_bar.tsx
@@ -3,24 +3,24 @@ import Image from "next/image";
 
 export default function NavBar() {
   return (
-    <div className="bg-bg-primary/85 mt-5 hidden w-full items-center justify-between p-2 shadow-[0_2px_4px_-1px_rgba(0,0,0,0.15),0_-2px_12px_-1px_rgba(0,0,0,0.15)] transition-transform duration-300 lg:flex">
+    <nav className="bg-bg-primary/85 mt-5 flex w-full items-center justify-between p-2 shadow-[0_2px_4px_-1px_rgba(0,0,0,0.15),0_-2px_12px_-1px_rgba(0,0,0,0.15)] transition-transform duration-300">
       <Link href="/">
         <Image
           src="/logo.png"
           alt="Website logo"
           width={130}
           height={0}
-          className="hidden w-full lg:block"
+          className="block w-full"
         />
       </Link>
-      <nav className="*:text-secondary flex w-auto justify-between gap-x-7 pr-10 *:py-5 *:text-center *:text-base *:uppercase *:transition-colors *:duration-300 *:hover:text-[#7dd3fc] *:active:text-[#1a1a6b] xl:gap-x-10 xl:*:text-xl *:dark:hover:text-[#ffffff] *:dark:active:text-[#38bdf8]">
+      <div className="*:text-secondary hidden w-auto justify-between gap-x-7 pr-10 *:py-5 *:text-center *:text-base *:uppercase *:transition-colors *:duration-300 *:hover:text-[#7dd3fc] *:active:text-[#1a1a6b] lg:flex xl:gap-x-10 xl:*:text-xl *:dark:hover:text-[#ffffff] *:dark:active:text-[#38bdf8]">
         <Link href="/about-us">about us</Link>
         <Link href="/curia">curia</Link>
         <Link href="/clergy">clergy</Link>
         <Link href="/pilgrimages">pilgrimages</Link>
         <Link href="/archdiocesan-institutions">archdiocesan institutions</Link>
         <Link href="/updates">updates</Link>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
