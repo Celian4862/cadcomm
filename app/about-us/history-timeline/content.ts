753d31a0158ba0d488a27672b6eb5d9331ff4b2e
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-06-17 12:22:17 +0800

    Added about-us/history-timeline/content.ts, updated about-us/history-timeline/page.tsx
    Made page.tsx more readable for developers and transferred all static content to content.ts.

---
+++ app/about-us/history-timeline/content.ts
@@ -0,0 +1,45 @@
+const content = [
+  {
+    name: "Gabriel Reyes",
+    image: "gabriel-reyes.png",
+    text: [
+      "Archbishop Gabriel M. Reyes was born on March 24, 1892, in Kalibo, Capiz, to Filipino parents. At the age of 13, he entered the Seminary of Jaro which was then directed by the Vincentian Fathers. Bishop Dennis Dougherty of Jaro, who later became Cardinal Archbishop of Philadelphia, U.S.A., ordained him on March 27, 1915, after which he was immediately appointed Coadjutor Parish Priest of the Cardinal becoming in a few months its Parish Priest.",
+      "As Parish Priest of Balasan (1915) he covered sixteen small islands many of which of neither roads nor chapels nor convents, but only ruins and an increasing number of Aglipayans and Protestants. These became the field of action of the energetic Gabriel until he became a Parish Priest of Capiz, Capiz (1918).",
+      "On July 20, 1820, Bishop James McClosky chose him to be the Diocesan Chancellor and Secretary and, at the same time, a Parish Priest of Sta. Barbara. In 1927, he became Vicar General of Jaro.",
+      "On October 11, 1932, Monsignor Gabriel Reyes was ordained Bishop of Cebu in the Cardinal of Jaro by the Apostolic Nuncio, Archbishop Guillermo Piani, S.D.B., with the assistance of Bishop James McClosky of Jaro and Bishop Alfredo Verzosa of Lipa. Two days later, he was installed in Cebu.",
+      "On April 28, 1934, Cebu, after more than three centuries was elevated by Pope Pius XI into an Archdiocese with Bishop Gabriel Reyes as the First Archbishop. As the Ecclesiastical Province of Santisimo Nombre de Jesus Cebu had for its suffragans in 1934 the following: the Diocese of Jaro (1865), Calbayog (1910), Zamboanga (1910), Bacolod (1932 detached from Manila), and Cagayan de Oro (1933).",
+      "With the able hands of Archbishop Gabriel Reyes, the Archdiocese began to embark on a series of noble projects designed to nurture the faith that had already been planted. In September of 1934, the weekly newspaper Lungsuranon appeared in its maiden issue. It was to help instruct the faithful on the fundamentals of the faith and to hold in check the advances of the other sectarian propaganda. The Archbishop encouraged the establishment of “Catholic Action” in the Parishes of Cebu and Bohol as well as the Catechesis for children and the Association of Propagation of Faith. He ordered the holding of Diocesan Eucharistic Congresses in all the Parishes of the Archdiocese for the preparation of the International Eucharistic Congress which was scheduled in Manila in February of 1937. In 1933 he established in Cebu the Adoracion Nocturna Filipina and in 1940 the Knights of Columbus. On July 27 - 29, 1937 he convoked the Third Diocesan Synod of Cebu where decrees adjusted to the new code of Canon Law (1918) were adopted. Truly a pastor of souls, Archbishop Reyes himself handled spiritual exercises for the common laborers, with hundreds of them attending every year.",
+      "Giving concrete responses to the evangelical imperative in works of mercy, Archbishop Reyes laid the foundation stone in 1939 for a large structure of the Asilo de Milagrosa to take care of the orphans and the aged. He supported the Association of St. Vincent de Paul to help the poor and the needy. Furthermore, he erected the now-defunct Holy Child Hospital with the same purpose in mind.",
+      "To mark the celebration of his Sacerdotal Silver Jubilee in 1940, Archbishop Reyes embellished the Cathedral erecting therein a marble altar which unfortunately did not survive the war. He encouraged religious vocation among the youth and ordained every year dozens of new candidates to the priesthood. He established the Parishes of Guadalupe, Tabuelan, Simala, and Santa Lucia to add to the existing parish, and launched a more vigorous evangelization program. He started the “Catholic Hour” over the radio station DZRC.",
+      "In 1945 the Treaty of Peace was signed. The Archbishop, pained almost to the point of nervous breakdown by the great destruction caused by the Second World War, such as the destruction of the Cathedral, his residence, the seminary, several churches, convents, and colleges, resigned to the will of God, commenced the work of reconstruction. His labor did not pass unrecognized for on August 25, 1949, he was appointed by the Holy Father Pius XII as Coadjutor to Archbishop Michael O'Doherty of Manila with right of succession (and Apostolic Administrator sede plena). With the death of the Archbishop, he took canonical possession of the archiepiscopal See of Manila with the distinction of being its first Filipino Archbishop (September 29, 1949).",
+      "Archbishop Gabriel Reyes has the distinction of being succeeded by two who have become Princes of the Church: Julio Cardinal Rosales who succeeded him in Cebu and Rufino Cardinal Santos who succeeded him in Manila.",
+    ],
+  },
+  {
+    name: "Julio Rosales",
+    image: "julio-rosales.png",
+    text: [
+      "Julio Rosales y Ras also known as Julio Rosales, the second Archbishop of Cebu, was a Filipino cardinal of the Roman Catholic Church.",
+      "A native of Calbayog, he made his studies at the Seminary of Calbayog and was ordained in his hometown on June 2, 1929.",
+      "From 1929 to 1946, he did pastoral work in the diocese of Calbayog. He was consecrated bishop of Tagbilaran on September 21, 1946.",
+      "On December 17, 1949, Rosales was promoted to the metropolitan see of Cebu. During his reign, in 1965, Cebu hosted the celebrations for the 400th anniversary of the Christianization of the Philippines.",
+      "Rosales was elevated to the College of Cardinals by Pope Paul VI in the consistory of April 28, 1969, and given the titular church of Sacro Cuore di Gesù agonizzante a Vitinia. He also participated in both conclaves of 1978. He resigned the pastoral government of his archdiocese, on August 24, 1982, and died less than a year later in Cebu City, his beloved episcopal city.",
+      "Julio Rosales is buried at the mausoleum of the Roman Catholic Metropolitan Cathedral in Cebu City, Cebu, Philippines. His mementos are currently on display at the Cathedral Museum of Cebu.",
+    ],
+  },
+  {
+    name: "Ricardo Vidal",
+    image: "ricardo-vidal.png",
+    text: [
+      "Cardinal Ricardo J. Vidal, Archbishop Emeritus of Cebu (Philippines), was born on 6 February 1931 in Mogpoc, Philippines. He did his studies at the minor seminary of the Most Holy Rosary (which later assumed the title of Our Lady of Carmel) and at the seminary of San Carlo.",
+      "He was ordained on 17 March 1956. The Bishop of Lucena entrusted him as spiritual director of the local seminary of Mount Carmel. He then became superior of the same institute and was dedicated to the formation of the young candidates to priesthood until 10 September 1971, when he was named Coadjutor Bishop of Malolos, Bulacan, and was elected to the titular church of Claterna. He received episcopal ordination on 30 November 1971. On 22 August 1973, he was named Archbishop of Lipa in Batangas.",
+      "On 13 April 1981, he was named Coadjutor with the right of succession to the Archbishop of Cebu, Cardinal Julio Rosales. He was named Archbishop on 24 August 1982.",
+      "He served as president of the Bishops' Commission for Vocations within the Catholic Bishops' Conference of the Philippines. He was also vice-president of the Catholic Bishops' Conference and then president from 1985 to 1987.",
+      "Archbishop Emeritus of Cebu (Philippines), 15 October 2010.",
+      "He participated in the conclave of April 2005, which elected Pope Benedict XVI.",
+      "Created and proclaimed Cardinal by St. John Paul II in the Consistory of 25 May 1985, of the Title of Ss. Pietro e Paolo in Via Ostiense.",
+    ],
+  },
+];
+
+export default content;
