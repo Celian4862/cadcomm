08c83ec2946159b1fe7de12c8f38bb93c4b2119c
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-06-18 21:50:53 +0800

    Updated home_button.tsx
    Turned the component into a client component and added an onClick event handler to the button.

--- app/ui/home_button.tsx
+++ app/ui/home_button.tsx
@@ -1,7 +1,18 @@
-export default function ReadMore({ className = "" }: { className?: string }) {
+"use client";
+
+export default function ReadMore({
+  href = "",
+  className = "",
+}: {
+  href?: string;
+  className?: string;
+}) {
   return (
     <button
       className={`${className} hover:bg-bg-primary relative my-6 rounded-4xl border border-solid border-(--foreground) bg-(--background) px-8 py-3 text-xs font-extrabold tracking-widest uppercase shadow-[0_3px_2px_var(--button-shadow)] active:top-0.5 active:shadow-none`}
+      onClick={() => {
+        window.location.href = href;
+      }}
     >
       Read more
     </button>
