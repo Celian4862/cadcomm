534c1c0d57d7ab7f011e871bc0a52431fe63dede
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-06-19 12:18:27 +0800

    Updated home_button.tsx
    Changed the button to a Next Link tag to allow the client to still use the link even with JS disabled.

--- app/ui/home_button.tsx
+++ app/ui/home_button.tsx
@@ -1,4 +1,4 @@
-"use client";
+import Link from "next/link";
 
 export default function ReadMore({
   href = "",
@@ -8,13 +8,11 @@ export default function ReadMore({
   className?: string;
 }) {
   return (
-    <button
-      className={`${className} hover:bg-bg-primary relative my-6 rounded-4xl border border-solid border-(--foreground) bg-(--background) px-8 py-3 text-xs font-extrabold tracking-widest uppercase shadow-[0_3px_2px_var(--button-shadow)] active:top-0.5 active:shadow-none`}
-      onClick={() => {
-        window.location.href = href;
-      }}
+    <Link
+      href={href}
+      className={`${className} hover:bg-bg-primary relative my-6 inline-block rounded-4xl border border-solid border-(--foreground) bg-(--background) px-8 py-3 text-xs font-extrabold tracking-widest uppercase shadow-[0_3px_2px_var(--button-shadow)] active:top-0.5 active:shadow-none`}
     >
       Read more
-    </button>
+    </Link>
   );
 }
