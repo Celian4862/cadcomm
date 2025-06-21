a023ab0c03637a5012c78915e7bfff3abc5c83e4
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-06-21 14:32:06 +0800

    Copied about-us links to about-us/layout.tsx

--- app/about-us/layout.tsx
+++ app/about-us/layout.tsx
@@ -1,5 +1,14 @@
 import SidebarNav from "@/ui/SidebarNav";
 
+const links = [
+  { href: "/about-us/history-timeline", label: "History Timeline" },
+  { href: "/about-us/archbishop-of-cebu", label: "Archbishop of Cebu" },
+  {
+    href: "/about-us/archdioceses-coat-of-arms",
+    label: "Archdiocese's Coat of Arms",
+  },
+];
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="p-5 lg:grid lg:grid-cols-4 lg:gap-5">
