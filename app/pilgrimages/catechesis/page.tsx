b184745223d5de2a4578aaec23d504debe2b503b
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-06-21 15:23:35 +0800

    Added new pages for Pilgrimages

---
+++ app/pilgrimages/catechesis/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <>
+      <header className="mx-6 my-10">
+        <h1 className="text-tertiary text-7xl font-extrabold uppercase">
+          catechesis
+        </h1>
+      </header>
+    </>
+  );
+}
